d8fea69f5ce68f7c4399bb2e98371cbaaeac9ecf
Tani-2007 <saxenatanishq4567@gmail.com>
2025-05-12 23:46:41 +0530

    first

---
+++ section7/src/Pojo.java
@@ -0,0 +1,7 @@
+public class Pojo {
+    //todo POJO-->A plain old java object is a class that generally have only one instance fields.
+    //todo  it is used to house data and pass data b/w functional classes
+    //todo it usually has no other way or very few methods,other than getter and setters for the instance fields .
+    //!it is generally used to write data and read data from/to database.
+    //? see example in Student class.
+}
